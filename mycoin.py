a784f718d9f43cff0f21d57e3aec1a13b1738962
FuryAryan <yoyo26new@gmail.com>
2020-03-30 21:21:52 +0530

    made changes in mycoin.py

--- mycoin.py
+++ mycoin.py
@@ -154,6 +154,16 @@ def is_valid():
     else:
         response = {'message': 'Blockchain is Invalid '}
     return jsonify(response), 200
+#Adding a new transaction to the Blockchain 
+@app.route('/add_transaction', methods = ['POST'] )
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender','receiver','amount']        #to check all the keys in json file are present
+    if not all (key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 404
+    index = blockchain.add_transaction(json['sender'], json['receiver'],json['amount'])
+    response = {'message': f'This transaction will be added to Block (index)'}
+    return jsonify(response), 201
 # Decentralizing Blockchain 
 
 
